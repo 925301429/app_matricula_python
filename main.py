d0e04d6b4c72137f8eb3cac05fcdf23c2e5b278c
lisbet_zevallos <marianazevallos9@gmail.com>
2024-10-14 10:16:01 -0500

    suvo la clase del lunes 14 de octubre

--- main.py
+++ main.py
@@ -3,26 +3,47 @@
 - generar fichaa de matricula
 - mostrar las listas de todoslos matriculados
 - filtrar matriculados por programa de estudio
+
 """
 lista_alumnos=[]
-#inicio del ptoblema
-#necesito ´poder ageregar mas alumnos sin necesidad de crear tantas variables
-#posible solucion cree o encerrar el codigoen un ciclo while
+def mensaje_menu():
+    menu_opciones="""
+        --------bienvenidos al sistema-------
+            ----Registra tu alumno-----
+    1.Escribe (s) si deceas agregar un nuevo alumno
+    2.Escribe (n) si deceas salir del sistema
+    Escribe la accion que decea realiza:"""      
+    return menu_opciones 
 
-nombre=input("Ingrese el nombre del alumno: ")
-apellido=input("Ingrese el apellido del alumno: ")
-nombre2=input("Ingrese el nombre del alumno: ")
-apellido2=input("Ingrese el apellido del alumno: ")
-alumnos={
-    "nombre" :nombre,
-    "apellido" :apellido
-}
-alumnos2={
-    "nombre" :nombre2,
-    "apellido" :apellido2
-}
-lista_alumnos.append(nombre)
-lista_alumnos.append(apellido2)
-# fin delproblema
-# deceo mostrar un menu con las opciones de agregar un menu alumno y salir del programa                                 
-print(lista_alumnos)
+def ingresar_datos_alumnos()
+    id=len(lista_alumnos)+1
+    cui=int(input("ingrese el numero de su dni:")) 
+    nombre=input("ingrese elnombre delalumno:")
+    apellido=input("ingrese el apellido del alumno:")
+    numero_celular=int(input("ingrese su numero de celular:"))
+    programa_estudio=imput("ingrese su ciclo academico:")
+    ciclo_academico=input("ingrese su ciclo academico:")
+    email=input("ingrese su correo electronico:")
+def guarda_datos_alumno(id,cui,nombre,apellido,numero_celular,programa_estudio,ciclo_academico,email):
+    alumnos={
+        "id":id,
+        "cui":cui,
+        "nombre":nombre,
+        "apellido":apellido,
+        "numero_celular":numero_celular,
+        "programa_estudio":programa_estudio,
+        "ciclo_academico":ciclo_academico,
+        "email":email
+    }
+while True:
+    menu_opciones=input(mensaje_menu())
+    if menu_opciones.lower() == "n":
+        print("saliendo del sistema  ")
+        break
+    elif menu_opciones.lower() == "s":
+        ingresar_datos_alumnos()
+    else:
+        print("opciones erroneas")
+print (lista_alumnos)
+
+
